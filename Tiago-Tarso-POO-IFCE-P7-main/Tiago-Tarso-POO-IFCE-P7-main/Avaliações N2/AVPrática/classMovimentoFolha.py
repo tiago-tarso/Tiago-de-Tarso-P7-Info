5eaa8650dae095b22f97eb9c8f33437aa22ad032
tiago-tarso <80657485+tiago-tarso@users.noreply.github.com>
2021-07-15 15:16:38 -0300

    Delete Tiago-Tarso-POO-IFCE-P7-main/Tiago-Tarso-POO-IFCE-P7-main/Avaliações N2/AVPrática directory

--- Tiago-Tarso-POO-IFCE-P7-main/Tiago-Tarso-POO-IFCE-P7-main/Avaliações N2/AVPrática/classMovimentoFolha.py
+++
@@ -1,7 +0,0 @@
-class MovimentoFolha:
-
-    def __init__(self, codigoColab, descricao, valor, tipoMovimento):
-        self.codigoColab = codigoColab
-        self.descricao = descricao
-        self.valor = valor
-        self.tipoMovimento = tipoMovimento
--- Tiago-Tarso-POO-IFCE-P7-main/Tiago-Tarso-POO-IFCE-P7-main/Avaliações N2/AVPrática/classTipoMovimento.py
+++
@@ -1,5 +0,0 @@
-import enum
-
-class TipoMovimento(enum.Enum):
-    P = 1
-    D = 2
